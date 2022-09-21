6570d9a7a54ca6ddb63314ffd7f94a6a27914c29
Mohamed Akaddar <78790925+elakaddo@users.noreply.github.com>
2022-09-21 19:30:49 +0200

    Delete livraison/doc directory

--- livraison/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"demoPackage"},{"l":"model"},{"l":"vue.vueConsole"},{"l":"vue.vueGraphique"}];updateSearchResults();
